81aacf8f27413ce8638cdab68d6a09a3880c365a
Corentin Barreau <corentin.barreau24@gmail.com>
2024-12-11 10:50:43 +0100

    add: reset only 1 URL

--- reset.go
+++ reset.go
@@ -33,3 +33,32 @@ func (c *Client) Reset() (err error) {
 
 	return nil
 }
+
+func (c *Client) ResetURL(ID string) (err error) {
+	expectedStatusCode := 200
+
+	req, err := http.NewRequest(http.MethodPost, c.ResetEndpoint.String()+"/"+ID, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("X-Auth-Key", c.Key)
+	req.Header.Add("X-Auth-Secret", c.Secret)
+	req.Header.Add("User-Agent", "gocrawlhq/"+Version)
+
+	if c.Identifier != "" {
+		req.Header.Add("X-Identifier", c.Identifier)
+	}
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != expectedStatusCode {
+		return fmt.Errorf("non-%d status code: %d", expectedStatusCode, resp.StatusCode)
+	}
+
+	return nil
+}
